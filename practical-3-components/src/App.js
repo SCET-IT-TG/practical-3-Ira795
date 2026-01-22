0c4fa0f75d7fa313381a4bbfd893f9518c7a4e30
Ira Ghelani <iraghelani.it23d1@scet.a.in>
2026-01-22 15:18:15 +0530

    IG

--- practical-3-components/src/App.js
+++ practical-3-components/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Counter from './components/Counter';
 import TweetInput from './components/TweetInput';
 import ThemeSwitcher from './components/ThemeSwitcher';
+import './App.css';
 
 function App() {
     return (
